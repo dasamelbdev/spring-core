33adfc68556662680d0c44cd6416bf2531dfa706
dasun <dasun.samarasiri@shipxpress.com>
2017-03-21 22:45:58 +0530

    commiting the empty project

---
+++ manuala-dependency-injection-demo/src/main/java/com/dasun/spring_practice/manuala_dependency_injection_demo/App.java
@@ -0,0 +1,13 @@
+package com.dasun.spring_practice.manuala_dependency_injection_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
