eb28a0efb119ea43394e70d68578ff3d6d87a7c1
dasun <dasun.samarasiri@shipxpress.com>
2017-05-13 04:03:43 +0530

    object collection injection using setters and util namespace

--- spring-collection-injection/src/main/java/App.java
+++ spring-collection-injection/src/main/java/App.java
@@ -1,4 +1,7 @@
+import com.spring.practice.model.CustomTypeCollectionHolder;
 import com.spring.practice.model.SimpleCollectionHolder;
+import com.spring.practice.model.Student;
+import com.spring.practice.model.UtilNamespaceCollectionHolder;
 import org.apache.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -21,6 +24,9 @@ public class App {
 
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("META-INF/application-context.xml");
         SimpleCollectionHolder simpleCollectionHolder = applicationContext.getBean("simpleCollectionHolderId", SimpleCollectionHolder.class);
+        CustomTypeCollectionHolder customTypeCollectionHolder = applicationContext.getBean("customTypeCollectionHolderId", CustomTypeCollectionHolder.class);
+        UtilNamespaceCollectionHolder utilNamespaceCollectionHolder = applicationContext.getBean("utilNamespaceCollectionHolderId", UtilNamespaceCollectionHolder.class);
+
         log.info("========printing injected list============");
         List<String> list = simpleCollectionHolder.getList();
         for (String s : list) {
@@ -36,6 +42,50 @@ public class App {
         for (Map.Entry<String, String> entry : map.entrySet()) {
             log.info("key : " + entry.getKey() + " value :" + entry.getValue());
         }
+
+
+
+
+
+
+        log.info("====================printing object collections injected using setters======================");
+        log.info("========printing injected list============");
+        List<Student> studentList = customTypeCollectionHolder.getList();
+        for (Student s : studentList) {
+            log.info(s);
+        }
+        log.info("========printing injected set============");
+        Set<Student> studentSet = customTypeCollectionHolder.getSet();
+        for (Student s : studentSet) {
+            log.info(s);
+        }
+        log.info("========printing injected map============");
+        Map<String, Student> studentMap = customTypeCollectionHolder.getMap();
+        for (Map.Entry<String, Student> entry : studentMap.entrySet()) {
+            log.info("key : " + entry.getKey() + " value :" + entry.getValue());
+        }
+
+
+
+
+
+        log.info("====================printing object collections injected using  util namespace======================");
+        log.info("========printing injected list============");
+        List<Student> studentUtilList = utilNamespaceCollectionHolder.getList();
+        for (Student s : studentUtilList) {
+            log.info(s);
+        }
+        log.info("========printing injected set============");
+        Set<Student> studentUtilSet = utilNamespaceCollectionHolder.getSet();
+        for (Student s : studentUtilSet) {
+            log.info(s);
+        }
+        log.info("========printing injected map============");
+        Map<String, Student> studentUtilMap = utilNamespaceCollectionHolder.getMap();
+        for (Map.Entry<String, Student> entry : studentUtilMap.entrySet()) {
+            log.info("key : " + entry.getKey() + " value :" + entry.getValue());
+        }
+
         ((ClassPathXmlApplicationContext) applicationContext).close();
 
     }
---
+++ spring-collection-injection/src/main/java/com/spring/practice/model/CustomTypeCollectionHolder.java
@@ -0,0 +1,41 @@
+package com.spring.practice.model;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
+/**
+ * Created by dasun on 5/13/17.
+ */
+public class CustomTypeCollectionHolder {
+
+    private List<Student> list;
+    private Set<Student> set;
+    private Map<String, Student> map;
+
+
+    public List<Student> getList() {
+        return list;
+    }
+
+    public void setList(List<Student> list) {
+        this.list = list;
+    }
+
+    public Set<Student> getSet() {
+        return set;
+    }
+
+    public void setSet(Set<Student> set) {
+        this.set = set;
+    }
+
+    public Map<String, Student> getMap() {
+        return map;
+    }
+
+    public void setMap(Map<String, Student> map) {
+        this.map = map;
+    }
+}
