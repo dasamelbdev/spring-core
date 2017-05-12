689bf6988f150bf7dfb7aa0da680e797d266fa15
dasun <dasun.samarasiri@shipxpress.com>
2017-05-13 01:42:22 +0530

    adding simple collection inject example

---
+++ spring-collection-injection/src/main/java/com/spring/practice/model/SimpleCollectionHolder.java
@@ -0,0 +1,50 @@
+package com.spring.practice.model;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
+/**
+ * Created by dasun on 5/12/17.
+ */
+public class SimpleCollectionHolder {
+
+    private List<String> list;
+    private Set<String> set;
+    private Map<String,String> map;
+    private Properties properties;
+
+
+    public List<String> getList() {
+        return list;
+    }
+
+    public void setList(List<String> list) {
+        this.list = list;
+    }
+
+    public Set<String> getSet() {
+        return set;
+    }
+
+    public void setSet(Set<String> set) {
+        this.set = set;
+    }
+
+    public Map<String, String> getMap() {
+        return map;
+    }
+
+    public void setMap(Map<String, String> map) {
+        this.map = map;
+    }
+
+    public Properties getProperties() {
+        return properties;
+    }
+
+    public void setProperties(Properties properties) {
+        this.properties = properties;
+    }
+}
